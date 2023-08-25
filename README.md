e68e295698ad9a8fa01dd8f47757e674af844e49
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-08-25 17:12:32 +0300

    problems

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Two Sum
+2. Palindrome Number
+3. Roman to Integer
+4. Longest Common Prefix
+5. Merge Two Sorted lists
---
+++ arrays_and_hashing/1-two_sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+}
